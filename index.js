4ac99755071215819686d578d9c1e84eae59c7be
Adedamola Ajasa <azeemajasa@gmail.com>
2023-08-06 08:14:28 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+// Your code goes here  
+// const paragraphContent = document.querySelector('p')
+
+// const DOMContentLoaded = document.addEventListener("DOMContentLoaded", function(){
+//     console.log("The DOMContent has been loaded.")
+// })
+
+document.addEventListener("DOMContentLoaded", function() {
+  const paragraphElement = document.querySelector("#text");
+  paragraphElement.textContent = "This is really cool!";
+});
